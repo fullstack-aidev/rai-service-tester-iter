5062650efb5f4c69e10a6637befbdfb94876042c
fullstack-aidev <ndradev@protonmail.com>
2025-01-18 09:35:01 +0700

    init commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod config_loader;
 
-use std::process::{Command, exit};
 use std::thread;
 use std::time::Duration;
 use reqwest::blocking::Client;
@@ -9,20 +8,9 @@ use config_loader::load_config;
 fn main() {
     let config = load_config("config_iter.yml");
 
-    let service_name = "rai_service_tester";
     let client = Client::new();
 
     for _ in 0..config.loop_count {
-        let status = Command::new("docker-compose")
-            .args(&["up", "-d", &service_name])
-            .status()
-            .expect("failed to execute docker-compose");
-
-        if !status.success() {
-            eprintln!("Failed to start service {}", service_name);
-            exit(1);
-        }
-
         // Wait for the specified delay before proceeding to the next iteration
         thread::sleep(Duration::from_millis(config.delay_between_iteration_in_ms));
 
@@ -34,7 +22,7 @@ fn main() {
 
             if !response.status().is_success() {
                 eprintln!("Failed to start client at {}: {:?}", address, response.status());
-                exit(1);
+                std::process::exit(1);
             }
         }
     }
