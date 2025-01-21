2d16b13e6782f17b05a161f7c8bf4ad27874819a
fullstack-aidev <ndradev@protonmail.com>
2025-01-21 21:34:33 +0700

    init commit

--- Dockerfile
+++ Dockerfile
@@ -2,19 +2,19 @@
 FROM rust:latest
 
 # Set the working directory inside the container
-WORKDIR /usr/src/app
+WORKDIR /usr/src/rai-service-tester
 
-# Copy the Cargo.toml and Cargo.lock files
+# Copy only the necessary files and directories for the build into the container at /usr/src/rai-service-tester
 COPY Cargo.toml Cargo.lock ./
+COPY src ./src
+COPY config_iter.yml ./
 
-# Build the dependencies
+
+# Build the Rust application
 RUN cargo build --release
 
-# Copy the source code into the container
-COPY . .
+# Expose the port that the application will run on
+EXPOSE 3030
 
-# Build the application
-RUN cargo build --release
-
-# Set the command to run the application
+# Run the application in servertest mode
 CMD ["./target/release/rai-service-tester-iter"]
--- src/main.rs
+++ src/main.rs
@@ -43,53 +43,36 @@ async fn main() {
 }
 
 async fn run_iterations(client: Client, config: Config) {
+    info!("Starting run_iterations function");
+
     // Add a delay to ensure the server is ready
     sleep(Duration::from_secs(10)).await;
+    info!("Initial delay completed");
 
     for i in 0..config.loop_count {
         info!("Starting loop iteration {}", i + 1);
 
         // Wait for the specified delay before proceeding to the next iteration
         sleep(Duration::from_millis(config.delay_between_iteration_in_ms)).await;
+        info!("Delay between iterations completed");
 
         // Create a vector to hold all the request futures
         let mut requests = Vec::new();
 
         // Make an HTTP request to the start route of each replica
-        for address in &config.rai_service_tester.replica_address {
-            info!("Sending request to {}", address);
-            let client = client.clone();
-            let address = address.clone();
-            let request = async move {
-                let mut retries = 5;
-                while retries > 0 {
-                    let response = client.post(&format!("{}/start", address)).send().await;
-                    match response {
-                        Ok(resp) => {
-                            if resp.status().is_success() {
-                                info!("Successfully started client at {}", address);
-                                break;
-                            } else {
-                                error!("Failed to start client at {}: {:?} - {:?}", address, resp.status(), resp.text().await);
-                            }
-                        }
-                        Err(err) => {
-                            error!("Failed to send request to {}: {:?}", address, err);
-                        }
-                    }
-                    retries -= 1;
-                    if retries > 0 {
-                        error!("Retrying in 3 seconds...");
-                        sleep(Duration::from_secs(3)).await;
-                    } else {
-                        error!("Giving up on {}", address);
-                    }
-                }
-            };
+        for (index, address) in config.rai_service_tester.replica_address.iter().enumerate() {
+            let service_name = format!("rai_service_tester_{}", index + 1);
+            let address = address.replace("localhost", &service_name);
+            let url = format!("{}/start", address);
+            info!("Sending request to {}", url);
+            let request = client.post(&url).send();
             requests.push(request);
         }
 
         // Wait for all requests to complete
         join_all(requests).await;
+        info!("All requests for iteration {} completed", i + 1);
     }
+
+    info!("Completed all iterations");
 }
